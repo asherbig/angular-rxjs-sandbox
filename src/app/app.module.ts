df65b6ea28983db93f0eff0b7b1dbcae7ee12be2
asherbig <aherbigs@gmail.com>
2021-04-21 14:30:45 -0400

    Add people table skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { ApiService } from "./services/api.service";
+import { PeopleTableComponent } from "./components/people-table/people-table.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule, FormsModule],
+  declarations: [AppComponent, PeopleTableComponent],
   bootstrap: [AppComponent],
   providers: [ApiService]
 })
